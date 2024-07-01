162b95d8226ac11907edbe06e7e88f7f5e3ca3af
daviventura <155470809+daviventura@users.noreply.github.com>
2024-06-30 23:14:29 -0300

    Update dashboard_st2.py

--- dashboard_st2.py
+++ dashboard_st2.py
@@ -85,7 +85,7 @@ st.header('VALOR DE MERCADO MUNDIAL')
 st.metric('Maior Valor de Mercado',
             value=str(max(vm_previsto))+' Milhões € - '+maior_vm_pais)
 
-with st.container(height=500):
+with st.container(height=400):
     st.map(df_map,latitude='Latitude',longitude='Longitude',size='Size',use_container_width=False)
 
 
