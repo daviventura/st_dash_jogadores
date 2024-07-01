e4bceeaddeaf9813844de4a1d9f4dd74f21a4e52
daviventura <155470809+daviventura@users.noreply.github.com>
2024-06-30 23:45:33 -0300

    Update dashboard_st2.py

--- dashboard_st2.py
+++ dashboard_st2.py
@@ -92,10 +92,10 @@ col2_2.map(df_map,latitude='Latitude',longitude='Longitude',size='Size',use_cont
 
 # GRÁFICOS
 
-with col3_3.container(height=400,border=True):
-
+with col3_3.container(height=200,border=True):
     st.subheader('Valor de Mercado Previsto - Em € Milhões')
     st.bar_chart(df_bar_lr)
 
+with col3_3.container(height=200,border=True):
     st.subheader('Probabilidade Venda / País (%)')
     st.bar_chart(df_bar_sm)
